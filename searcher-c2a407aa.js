63910eb2022059734f3ad00479daeb6e91024585
AlexandrTolstuhin <AlexandrTolstuhin@users.noreply.github.com>
2026-03-20 04:03:37 +0000

    deploy: 3d61f096c8bfd628e5864bf1702bfcf13f619495

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-3ab6fb17.js',
+                path_to_root + 'searchindex-ecd4aa31.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
